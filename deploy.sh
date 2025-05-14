16344d84c5f7982901f3e6fc3f001ae11d7dbdac
olaj20 <juicedbeats20@gmail.com>
2025-05-14 19:35:03 +0000

    merging compelled

--- deploy.sh
+++ deploy.sh
@@ -2,3 +2,6 @@ echo java apps
 echo python applicationsecho java apps
 echo python applicationsecho java apps
 echo python applications
+echo python applicationstestin 
+testing incoming
+team testing approved
